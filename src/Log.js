b876ca89647fe9735ec41f845e85bf1c5a1e1aa1
Beth-A <elizabeth78@me.com>
2021-01-04 00:04:13 -0500

    init commit

---
+++ src/Log.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Entries from './Entries';
+
+const Log = ({ items }) => {
+    //Initilize State
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    //helper function
+    const onTitleClicked = (index) => {
+        setActiveIndex(index)
+    };
+
+    const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+
+        return (
+            <React.Fragment key={item.title}>
+                <div 
+                    className={`title ${active}`}
+                    onClick={() => onTitleClicked(index)}
+                >
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className={`content ${active}`}>
+                    <p> {item.content} </p>
+                </div>
+            </React.Fragment>
+        )
+    })
+
+    return (
+        <div className="ui container">
+            <div className="ui styled accordion">
+                {renderedItems}
+            </div>
+        </div>
+        
+    )
+};
+
+export default Log;
