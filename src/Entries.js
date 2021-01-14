a5e642d27b53d028e5e9f8e96bac9551ccafd295
Beth-A <elizabeth78@me.com>
2021-01-14 14:20:30 -0500

    added entries for 1/12/2021-1/14/2021

--- src/Entries.js
+++ src/Entries.js
@@ -42,7 +42,7 @@ const Entries = [
     {
         title: 'Jan 5, 2021',
         content: {
-            p1: 'So I thought about the problem I had yesterday, all night & morning long. I decided that since I could see the previous committed code in my repo history, I would re-create the files.  So that is what I did. I went thru the painfully slow process of re-creating everything.  I ran it to make sure it was working correctly without being deployed. Then pushed it up to up to GitHub. All looks great! So I am going to deploy it, and all should be well again, right? WRONG!!! I deployed it, and all of my files disappeared just like they did yesterday!?!?!? I double-check everything! I do not understand how this is happening! ', 
+            p1: 'So I thought about the problem I had yesterday, all night & morning long. I decided that since I could see the previous committed code in my repo history, I would re-create the files.  So that is what I did. I went thru the painfully slow process of re-creating everything.  I ran it to make sure it was working correctly without being deployed. Then pushed it up to GitHub. All looks great! So I am going to deploy it, and all should be well again, right? WRONG!!! I deployed it, and all of my files disappeared just like they did yesterday!?!?!? I double-check everything! I do not understand how this is happening! ', 
 
             p2:'Again, I went thru the process of trying to revert the code.  It does not revert again! So I skip to re-creating the files a 2nd time. Test it, working correctly. All files are in the repo. I deploy it and lose everything again!!!!',
 
@@ -50,7 +50,7 @@ const Entries = [
 
             p4: 'I had also noticed that my repo was acting weird. Like it would not show me all of my commits every time. Sometimes it would only show four commits instead of the 16 that should be showing. So then I got to wondering if maybe I had done something in all of my attempts to revert/restore/recreate that I corrupted my repo. So I did what any self-respecting Dev would do, I built a new repo to re-create my project in it.',
 
-            p5: 'I copied the same code over into the new repo from the commit history that I had been using. I tested it, and it worked. I then added the deployment code for GH Pages. Deployed the app, and it deployed properly on the 1st attempt!!!!!!! *hallelujah*',
+            p5: 'I copied the same code over into the new repo from the commit history that I had been using. I tested it, and it worked. I then added the deployment code for GH Pages. I deployed the app, and it deployed properly on the 1st attempt!!!!!!! *hallelujah*',
 
             p6: 'I still do not know what happened in the 1st repo or what went wrong when I tried to deploy it to Vercel, but it was a learning experience! Also, a great reminder that sometimes it is best to stick to what you know!'
         }
@@ -58,9 +58,9 @@ const Entries = [
     {
         title: 'Jan 6, 2021',
         content: {
-            p1: 'Today I spent the bulk of my day working on minor fixes.  I changed the booter styling on several of my apps that have unified looks (Video Player, Picture Finder, Widgets, Coding Journal). I am trying to present several apps on my project site as unified learning pieces. I may eventually link them all together. If I do not link them, then I will need to at least edit the Headers to redirect them back to my website and add a contact me feature.',
-            p2: 'Once the styling enhancements had been made, I started in updating the deployed sites.  Video Player which gave me so much trouble the last 2 day, re-deployed with no problems! Picture Finder which originally deployed with no problems, is now giving me problems, similar to what I had the other day with Video Player. So I am not sure what I am doing wrong!',
-            p3: 'I also spent a period of time today detailing my experiences with Video Player the last 2 days.  I am getting it ready to publish on LI & either Medium or Dev.  It will be the 1st article that I have published, so I am rather nervous about that!'
+            p1: 'Today I spent the bulk of my day working on minor fixes.  I changed the booter styling on several of my apps with unified looks (Video Player, Picture Finder, Widgets, Coding Journal). I am trying to present several apps on my project site as harmonious learning pieces. I may eventually link them all together. If I do not connect them, I will need to edit the Headers to redirect them back to my website and add a "contact me" feature',
+            p2: 'Once I had made the styling enhancements, I started updating the deployed sites.  Video Player, which gave me so much trouble the last two days, re-deployed with no problems! Picture Finder, which initially deployed with no issues, is now giving me problems, similar to what I had the other day with Video Player. So I am not sure what I am doing wrong!',
+            p3: 'I also spent time today detailing my experiences with the Video Player app for the last two days.  I am getting it ready to publish on LI & either Medium or Dev.  It will be the 1st article that I have posted, so I am somewhat nervous about that!'
         }
     },
     {
@@ -109,9 +109,50 @@ const Entries = [
         }
     },
     {
+        title: 'Jan 12, 2021',
+        content: {
+            p1: 'Today was a networking day.  I did not code hardly at all, but I focused my time on attending a few meetings and building a stronger relationship with my LinkedIn connections. I know I should be coding, but I also realize that building a strong network of contacts is just as crucial to my career goals!',
+            p2: 'As part of my focus on networking, I went through the invitations that I had sent out on LinkedIn and withdrew all of them more than four weeks old.  I am figuring if they are more than four weeks old, then they do not want to connect with me, are not active on LinkedIn any longer, or they did not see my invite come in. I ended up withdrawing over 800 invites. I want to make sure that I am targeting active connections on LinkedIn and are relevant to my career goals.',
+            p3: 'I am, but I am not focusing on the number of connections that I have. I know it is not about the actual number, but what that number represents.  I want a high connection count because I will have more exposure in the tech community and help others find me. I want to put myself in a position to be in demand and have others seeking me for roles rather than being the one to seek them out.'
+        }
+    },
+    {
+        title: 'Jan 13, 2021',
+        content: {
+            p1: 'I only got 3 hours of sleep last night & battling a migraine today, so no coding for me.  I did some reading on Sprint when the migraine allowed. Hopefully, tomorrow will be better!'
+        }
+    },
+    {
+        title: 'Jan 14, 2021',
+        content: {
+            p1: 'Today is a new & better day!  I woke up feeling recharged & ready to work! Yesterday, I thought about attacking my career goals best as they are split into two paths right now. So I have been studying and developing my skillsets as a Developer (Full-Stack & iOS), but I am also interested in being a PM. ',
+            p2: 'With my work history, being a PM might be a much better fit for me. I do enjoy development, but I do not have professional experience yet. I know I can get a job as a Developer eventually. But I have experience building and managing teams, so if I focus on that experience and add in my development skills, I could be an effective PM. Now I need to find the right company that is willing to give me a chance to prove myself!',
+            p3: 'So, I have decided that I will spend my mornings working on my PM skills and the afternoons working on my Dev skills.',
+            p4: 'I worked through two LinkedIn Learning courses as part of the "Becoming A Software Project Manager" learning path for this morning.  This learning path focuses on exploring project management fundamentals, learning how to implement agile and lean processes, and implementing best practices for software development. I believe this learning path will help me both as a PM and a Developer. ',
+            p5: 'As I am already a Certified Scrum Master, much of the fundaments are a refresher for me, but the learning path dives into advanced Scrum topics, DevOps Foundations, and Agile Testing.',
+            p6: 'This afternoon, I am diving back into the React and Redux course that I am taking. It is my 1st look into Redux, so I am excited to start with that today! So more to come on that later.'
+        }
+    },
+    {
         title: '',
         content: {
-            p1: ''
+            p1: '',
+            p2: '',
+            p3: '',
+            p4: '',
+            p5: '',
+            p6: ''
+        }
+    },
+    {
+        title: '',
+        content: {
+            p1: '',
+            p2: '',
+            p3: '',
+            p4: '',
+            p5: '',
+            p6: ''
         }
     },
 ]    
